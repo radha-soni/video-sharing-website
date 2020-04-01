3672cd487b6075d01012b1069b2520772e5e744b
radha-soni <er.radha17@gmail.com>
2020-04-01 13:45:14 +0530

    fix:navbar tab issue

--- src/components/SideNavBar.js
+++ src/components/SideNavBar.js
@@ -10,46 +10,41 @@ function SideNavBar() {
           src="https://www.travelmag.com/wp-content/uploads/2017/12/Headout-logo.png"
         />
       </div>
-      <div className="sidenavlinks">
-        <a href="/#">
-          <span className="iconhandler">
-            <i className="fa fa-fw fa-home"></i>
-          </span>
-          Home
-        </a>
-      </div>
-      <div className="sidenavlinks">
-        <a href="/#">
-          <span className="iconhandler">
-            <i className="fa fa-fw fa-fire"></i>
-          </span>
-          Trending
-        </a>
-      </div>
-      <div className="sidenavlinks">
-        <a href="/#">
-          <span className="iconhandler">
-            <i className="fa fa-fw fa-wrench"></i>
-          </span>
-          Settings
-        </a>
-      </div>
-      <div className="sidenavlinks">
-        <a href="/#">
-          <span className="iconhandler">
-            <i className="fa fa-fw fa-info-circle"></i>
-          </span>
-          Help
-        </a>
-      </div>
-      <div className="sidenavlinks">
-        <a href="/#">
-          <span className="iconhandler">
-            <i className="fa fa-comment"></i>
-          </span>
-          Feedback
-        </a>
-      </div>
+
+      <a href="/#">
+        <span className="iconhandler">
+          <i className="fa fa-fw fa-home"></i>
+        </span>
+        Home
+      </a>
+
+      <a href="/#">
+        <span className="iconhandler">
+          <i className="fa fa-fw fa-fire"></i>
+        </span>
+        Trending
+      </a>
+
+      <a href="/#">
+        <span className="iconhandler">
+          <i className="fa fa-fw fa-wrench"></i>
+        </span>
+        Settings
+      </a>
+
+      <a href="/#">
+        <span className="iconhandler">
+          <i className="fa fa-fw fa-info-circle"></i>
+        </span>
+        Help
+      </a>
+
+      <a href="/#">
+        <span className="iconhandler">
+          <i className="fa fa-comment"></i>
+        </span>
+        Feedback
+      </a>
 
       <button className="signinbtn">
         <span className="btnicon">
